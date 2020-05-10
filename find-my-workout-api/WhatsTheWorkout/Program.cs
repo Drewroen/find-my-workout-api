31e2cfd09700cf9d05a34eb6dcbdf0dd628d45c9
Drewroen <drewroen@gmail.com>
2020-05-10 08:46:38 -0500

    Initial commit

---
+++ find-my-workout-api/WhatsTheWorkout/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WhatsTheWorkout
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+        {
+            protected override void Init(IWebHostBuilder builder)
+            {
+                builder
+                    .UseStartup<Startup>();
+            }
+        }
+}
